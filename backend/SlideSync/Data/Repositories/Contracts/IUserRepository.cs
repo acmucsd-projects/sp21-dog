fa8d642a3db9072ba769956dcd360259b6a1ea02
Shane Kim <shanekim28@gmail.com>
2021-04-29 09:27:51 -0700

    Leaderboard route

--- backend/SlideSync/Data/Repositories/Contracts/IUserRepository.cs
+++ backend/SlideSync/Data/Repositories/Contracts/IUserRepository.cs
@@ -4,6 +4,7 @@ using SlideSync.Data.Entities;
 namespace SlideSync.Data.Repositories.Contracts {
     public interface IUserRepository {
         IEnumerable<UserModel> GetUsers();
+        IEnumerable<UserModel> GetLeaderboard(int count);
         UserModel GetUserById(int id);
         UserModel GetUserByUsername(string username);
         void AddUser(UserModel user);
