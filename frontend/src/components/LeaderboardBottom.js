dc777e095d35244428728aac78070b09cd40c581
erikajoun <erikajoun@gmail.com>
2021-05-04 15:36:22 -0700

    Fix font for bottom of leaderboard

--- frontend/src/components/LeaderboardBottom.js
+++ frontend/src/components/LeaderboardBottom.js
@@ -3,19 +3,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import Icon from '@material-ui/core/Icon'
 import { Color } from '../helpers/Color'
 const useStyles = makeStyles((theme) => ({
-    root: {
-        height: '60px',
-    },
-    title: {
-        flexGrow: 1,
-        fontSize: 36,
-        textTransform: 'capitalize',
-    },
-    topNavButton: {
-        '&:hover': {
-            cursor: 'pointer',
-        },
-    },
     imageIcon: {
         display: 'flex',
         height: 'inherit',
@@ -31,48 +18,37 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'left',
     },
     biggerText: {
+        fontSize: '18px',
         margin: 0,
         fontWeight: 700,
-        // fontFamily: 'PT Sans',
-        fontFamily: 'Helvetica',
     },
     smallerText: {
-        fontFamily: 'Helvetica',
-        fontSize: '0.85em',
+        fontSize: '14px',
         fontWeight: 400,
         margin: 0,
     },
 }))
-const LeaderboardBottom = () => {
+export default function LeaderboardBottom() {
     const classes = useStyles()
     return (
-        <div>
-            <AppBar position="static">
-                <div
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        backgroundColor: Color.accent,
-                    }}
-                >
-                    <Icon classes={{ root: classes.iconRoot }}>
-                        <img
-                            className={classes.imageIcon}
-                            src="/profilepic.svg"
-                        />
-                    </Icon>
-                    <div className={classes.container}>
-                        <p className={classes.biggerText}>
-                            You're in 2nd place with 19k pts!
-                        </p>
-                        <p className={classes.smallerText}>
-                            19k pts behind the next person
-                        </p>
-                    </div>
-                </div>
-            </AppBar>
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                backgroundColor: Color.accent,
+            }}
+        >
+            <Icon classes={{ root: classes.iconRoot }}>
+                <img className={classes.imageIcon} src="/profilepic.svg" />
+            </Icon>
+            <div className={classes.container}>
+                <p className={classes.biggerText}>
+                    You're in 2nd place with 19k pts!
+                </p>
+                <p className={classes.smallerText}>
+                    19k pts behind the next person
+                </p>
+            </div>
         </div>
     )
 }
-
-export default LeaderboardBottom
