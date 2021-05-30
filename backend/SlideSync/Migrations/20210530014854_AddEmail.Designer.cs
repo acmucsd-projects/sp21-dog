aeb33024fa3fe22f4ed10cee073c56cd59186fc2
shanekim28 <shanekim28@gmail.com>
2021-05-29 18:53:22 -0700

    Email hotfix (#69)
    * Edit profile
    
    * Edit password
    
    * Edit email
    
    * Documentation and hotfixes for tasks
    
    * Task titles and update task descriptions
    
    * Add level and number of completed tasks to user response
    
    Also dropped unneeded first and last name columns from db
    
    * Added Email column

--- backend/SlideSync/Migrations/20210530014854_AddEmail.Designer.cs
+++ backend/SlideSync/Migrations/20210530014854_AddEmail.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SlideSync.Data.Context;
 
 namespace SlideSync.Migrations
 {
     [DbContext(typeof(GameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530014854_AddEmail")]
+    partial class AddEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -107,7 +109,7 @@ namespace SlideSync.Migrations
 
                     b.Property<string>("DisplayName")
                         .HasColumnType("text");
-                        
+
                     b.Property<string>("Email")
                         .HasColumnType("text");
 
