5294bb5129818a4c73d236496e4522d3647caa89
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2021-05-31 20:44:14 -0700

    Mapbox CSS/Babel issues (#68)
    * changes from previous pr
    
    * change workerClass
    
    * added npm install
    
    * change workerClass name?
    
    * package.json update
    
    * maybe change eslint?
    
    * update eslint
    
    * rebase
    
    * Fix UI bugs
    
    Co-authored-by: erikajoun <erikajoun@gmail.com>

--- frontend/src/components/pages/tasks/Tasks.js
+++ frontend/src/components/pages/tasks/Tasks.js
@@ -24,7 +24,6 @@ export default function Tasks() {
         locationContext.setState({
             ...locationContext.state,
             viewportLocation: locationContext.state.userLocation,
-            zoom: '4',
         })
     }
 
